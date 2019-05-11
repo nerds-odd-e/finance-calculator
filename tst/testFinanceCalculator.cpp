536c3f260df6d6e241cd3584cf6410736460014f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-05-11 13:34:58 +0200

    :tada: init commit

---
+++ tst/testFinanceCalculator.cpp
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(FinanceCalculator){
+
+    void setup() {
+
+    }
+
+    void teardown() {
+
+    }
+};
+
+TEST(FinanceCalculator, test) {
+//    FAIL("Start from here.");
+}
