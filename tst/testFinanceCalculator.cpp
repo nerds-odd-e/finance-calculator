3d83b82180f57e1614fb6492e96819b7970852f6
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-05-11 14:16:36 +0200

    :sparkles: add implementation and tests for calculator

--- tst/testFinanceCalculator.cpp
+++ tst/testFinanceCalculator.cpp
@@ -12,6 +12,34 @@ TEST_GROUP(FinanceCalculator){
     }
 };
 
-TEST(FinanceCalculator, test) {
-//    FAIL("Start from here.");
+int plus(int a, int b) {
+    return a + b;
+}
+
+TEST(FinanceCalculator, plus) {
+    CHECK_EQUAL(2, plus(1, 1))
+}
+
+int minus(int a, int b) {
+    return a - b;
+}
+
+TEST(FinanceCalculator, minus) {
+    CHECK_EQUAL(4, minus(10, 6))
+}
+
+int multiple(int a, int b) {
+    return a * b;
+}
+
+TEST(FinanceCalculator, multiple) {
+    CHECK_EQUAL(15, multiple(3, 5))
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
+
+TEST(FinanceCalculator, divide) {
+    CHECK_EQUAL(2, divide(10, 5))
 }
